96c05a209937624d8570171d30a38467b20d5c03
jaysingh <thejaysinghjeemain2017@gmail.com>
2020-07-04 14:43:07 +0530

    Basic Authentication

---
+++ models/leaders.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const Schema = mongoose.Schema;
+
+const leaderSchema = new Schema({
+	name:{
+		type: String,
+		required: true,
+		unique: true
+	},
+	image:{
+		type: String,
+		required: true,
+	},
+	designation:{
+		type: String,
+		default: ''
+	},
+	abbr:{
+		type: String,
+        default: ''
+	},
+	description:{
+		type: String,
+		required: true
+	},
+	featured:{
+		type: Boolean,
+		default: false
+	}
+});
+
+const Leaders = mongoose.model('Leader', leaderSchema);
+module.exports = Leaders;
