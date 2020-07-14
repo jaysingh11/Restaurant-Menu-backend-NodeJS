8a885ae69050aeed56536acbcd44613bdae93c84
jaysingh <thejaysinghjeemain2017@gmail.com>
2020-07-14 18:54:37 +0530

    ready for github

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	'secretKey': '12345-67890-09876-54321',
+	'secretKey': 'your-key',
 	'mongoUrl': 'mongodb://localhost:27017/conFusion',
 	'facebook': {
-		clientId: '1689311831234255',
-		clientSecret: '066460827a6ea1b1a5afbf4193888f2f'
+		clientId: 'your clientId',
+		clientSecret: 'Your clientSecret'
 	}
 }
